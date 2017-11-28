b35d0c69cdb130dbb55d99b510b9c1fc7ba0633d
Hajin Song <hSong6230@gmail.com>
2017-11-28 21:38:44 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Audio Frequency
+
+Custom JavaScript package for manipulating Audio files through Web Audi API
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cleanTarget = ['dst'];
+
+module.exports = {
+ entry: ['./src/js/entry.js', './src/stylesheet/app.scss'],
+ output: {
+  filename: '[name].bundle.js',
+  path: path.resolve(__dirname, 'dst')
+ },
+ resolve: {
+  alias: {
+   'src': path.resolve(__dirname, 'src')
+  }
+ },
+ module: {
+  rules: [
+   { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+   {
+    test: /\.(scss|css|sass)$/,
+    exclude: /node_modules/,
+    use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+   }
+  ]
+ },
+ plugins: [
+  new CleanWebpackPlugin(cleanTarget),
+  new ExtractTextPlugin({
+   filename: '[name].css',
+   publicPath: path.resolve(__dirname, 'dst'),
+   allChunks: true
+  }),
+  new HtmlWebpackPlugin({
+   filename: path.resolve(__dirname, 'index.html'),
+   inject: 'head',
+   template: './src/html/index.html'
+  })
+ ]
+}
