1340642196871754af453ae658c0882a4c496aba
Hajin Song <hSong6230@gmail.com>
2018-07-11 09:03:20 +1000

    Attemtping to draw circle

--- src/js/Drawer.js
+++ src/js/Drawer.js
@@ -15,17 +15,28 @@ class Drawer {
         this.canvasCtx.lineWidth = 10;
         this.canvasCtx.strokeStyle = 'rgb(100, 100, 100)';
         this.canvasCtx.beginPath();
-
-        let sliceWidth = this.width * 1.0 / bufferLength;
+        this.canvasCtx.beginPath();
+        this.canvasCtx.arc(this.center.x, this.center.y, 5, 0, 2 * Math.PI);
+        this.canvasCtx.stroke();
+        let sliceWidth = bufferLength / this.circumference;
         let x = 0;
-        let prevY = this.height;
+        let prevX = 0;
+        let prevY = this.radius
         for(let i = 0 ; i < bufferLength ; i++){
             let v = dataArray[i] / 128.0;
-            let y = v * this.height/2;
-           
-            if(Math.abs(prevY - y) > 10){
-                i === 0 ? this.canvasCtx.moveTo(x, y) : this.canvasCtx.quadraticCurveTo(x, y, x - 0.5, (prevY + y) / 2);
-                prevY = y;
+            let y = v + this.radius;
+            let xCircle = Math.sin(x) * (this.radius);
+            //console.log(xCircle, y);
+            if(i === 0){
+                this.canvasCtx.moveTo(this.center.x, this.radius);
+            }else if(i < bufferLength/4){
+                this.canvasCtx.lineTo(this.center.x + xCircle, this.center.y + y);
+            }else if(i >= bufferLength/4 && i < (2*bufferLength)/4){
+                this.canvasCtx.lineTo(this.center.x + xCircle, this.center.y - y);
+            }else if(i >= (2*bufferLength)/4 && i > (3*bufferLength)/4){
+                this.canvasCtx.lineTo(xCircle - this.center.x, this.center.y - y);
+            }else {
+                this.canvasCtx.lineTo(xCircle - this.center.x, this.center.y + y);
             }
 
             
@@ -39,6 +50,9 @@ class Drawer {
         this.canvas.width = this.canvas.parentElement.clientWidth;
         this.height = this.canvas.height;
         this.width = this.canvas.width;
+        this.center = { 'x': this.width/2, 'y': this.height/2 };
+        this.radius = this.height < this.width ? this.height/4 : this.width/4;
+        this.circumference = 2 * Math.PI * this.radius;
     }
 }
 
